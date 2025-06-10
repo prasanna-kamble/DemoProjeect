b9b8f5dbff80adbd014f3aeb677b661e9b05b3f2
prasanna-kamble <prasannakamble08@gmail.com>
2025-06-10 20:16:12 +0530

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -4,19 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
+   
     WebDriver driver;
 
-    // Locators
     By usernameField = By.id("user-name");
     By passwordField = By.id("password");
     By loginButton = By.id("login-button");
 
-    // Constructor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Actions
     public void enterUsername(String username) {
         driver.findElement(usernameField).sendKeys(username);
     }
